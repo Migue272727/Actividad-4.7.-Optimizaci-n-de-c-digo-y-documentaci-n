187e7c971bd1947541d5f756aee5004d66a545b9
Miguel Angel <maceari2707@g.educaand.es>
2023-02-15 12:16:53 +0100

    Encapculate fields

--- src/Alumno/Alumno.java
+++ src/Alumno/Alumno.java
@@ -3,11 +3,49 @@ package Alumno;
 /**
  * Alumno.java
  * Definición de la clase Alumno
- * @author Luis José Sánchez
+ * @author Miguel Angek Acedo Ariza
+ * 
+ * Usando Encapsulate field, creamos los getter and setter
+ * de los atributos ademas de ponerlos como privados
+ * 
+ * Debido a que todos los atributos son privados y la forma
+ * de ver o añadir informacion a dichos atributos son los
+ * getter and setters
+ *  
  */
 
+
 public class Alumno {
-  public String nombre;
-  public double notaMedia = 0.0;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the notaMedia
+     */
+    public double getNotaMedia() {
+        return notaMedia;
+    }
+
+    /**
+     * @param notaMedia the notaMedia to set
+     */
+    public void setNotaMedia(double notaMedia) {
+        this.notaMedia = notaMedia;
+    }
+
+  private String nombre;
+  private double notaMedia = 0.0;
   
 }
